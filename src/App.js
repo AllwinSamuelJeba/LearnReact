98062343b820f53ca8aca43203102e610665a4d3
Jeba <allwin@gmail.com>
2021-10-23 21:26:33 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//Learning from https://www.youtube.com/watch?v=w7ejDZ8SWv8
+import Header from "./components/Header";
+
+function App() {
+
+  return (
+    <div className="container">
+<Header title="hello" />
+    </div>
+  );
+}
+
+export default App;
+   
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = (props) => {
+    return (
+        <header >
+            <h1 style={{color:'red', backgroundColor:'black'}}> {props.title}</h1>
+        </header>
+    )
+}
+
+export default Header
